94d1643b28fe7d0d5ee443653129a478e8b38bcc
thossapolud <76428440+thossapolud@users.noreply.github.com>
2020-12-21 18:22:05 +0700

    test
    test

---
+++ bot.php
@@ -0,0 +1,47 @@
+<?php
+// URL API LINE
+$API_URL = 'https://api.line.me/v2/bot/message';
+// ใส่ Channel access token (long-lived)
+$ACCESS_TOKEN = 'U6f621990573f6833c566bfd81e2f8193';
+// ใส่ Channel Secret
+$CHANNEL_SECRET = '943fd5cf2360ad7b306d183e4e83b0d4';
+
+// Set HEADER
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+// Get request content
+$request = file_get_contents('php://input');
+// Decode JSON to Array
+$request_array = json_decode($request, true);
+
+function send_reply_message($url, $post_header, $post_body)
+{
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+}
+
+if ( sizeof($request_array['events']) > 0 ) {
+    foreach ($request_array['events'] as $event) {
+    
+    $reply_message = '';
+    $reply_token = $event['replyToken'];
+    $data = [
+       'replyToken' => $reply_token,
+       'messages' => [
+          ['type' => 'text', 
+           'text' => json_encode($request_array)]
+       ]
+    ];
+    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+    $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+    echo "Result: ".$send_result."\r\n";
+ }
+}
+echo "OK";
